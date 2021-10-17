a93ae7de57855e13aab3ef9d75e049df85f70a7a
chrischenhui <e0322956@u.nus.edu>
2021-10-17 17:04:16 +0800

    Set up jwt authentication and authorisation backend

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require("../config/db");
 
 var createError = require('http-errors');
 var express = require('express');
@@ -6,13 +7,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
-require("../config/db");
 
+// Routes
+let authRoutes = require("./routes/auth-routes")
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require("./routes/testAPI");
 
 var app = express();
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, x-auth-token');
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,8 +35,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/testAPI", testAPIRouter);
+app.use('/', authRoutes)
+// app.use('/users', usersRouter);
+// app.use("/testAPI", testAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,6 +53,13 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+var port = process.env.PORT || 8081;
+
+// Launch app to listen to specified port
+app.listen(port, function () {
+     console.log("Running RestHub on port " + port);
 });
 
 module.exports = app;
