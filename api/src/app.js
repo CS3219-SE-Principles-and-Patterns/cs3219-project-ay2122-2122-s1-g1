e689a7836152271764382a361ee84dd7a7cedcb5
chrischenhui <e0322956@u.nus.edu>
2021-10-17 16:43:10 +0800

    Refactor folders

--- api/src/app.js
+++ api/src/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
-require("./config/db");
+require("../config/db");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
